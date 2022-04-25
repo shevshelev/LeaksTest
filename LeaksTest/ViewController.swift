31d979d1949a27e60d5457953fbe5d40ee3e10e9
Shevshelev Lev <shevshelev.lev@gmail.com>
2022-04-25 15:40:16 +0500

    Initial Commit

---
+++ LeaksTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeaksTest
+//
+//  Created by Shevshelev Lev on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
